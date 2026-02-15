bea3f300af4ff54905dae50c8b0ecb6ccfbda246
paulo <pauloricardsonsilvacosta@gmail.com>
2026-02-15 20:35:26 -0300

    Construção da Classe AgendaService, função de busca

--- src/main/java/br/mycontacts/service/AgendaService.java
+++ src/main/java/br/mycontacts/service/AgendaService.java
@@ -3,14 +3,25 @@ package br.mycontacts.service;
 import br.mycontacts.models.Contato;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AgendaService {
 
-    private ArrayList<Contato> contatoes;
+    private ArrayList<Contato> contatos;
 
     // 1. Adicionar novo Contato
+    public void adicionarContato(Contato contato) {
+        contatos.add(contato);
+    }
+
     // 2. Listar contatos
+    public List<Contato> listarContatos() {
+        return contatos;
+    }
+
     // 3. Buscar por nome
+
+
     // 4. Remover contato
 
 }
