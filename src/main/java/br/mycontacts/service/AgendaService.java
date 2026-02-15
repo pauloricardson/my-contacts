8d36afb024d16e89dcf00024478705da965e94ae
paulo <pauloricardsonsilvacosta@gmail.com>
2026-02-15 20:43:32 -0300

    Construção da Classe AgendaService, função de busca

--- src/main/java/br/mycontacts/service/AgendaService.java
+++ src/main/java/br/mycontacts/service/AgendaService.java
@@ -2,6 +2,7 @@ package br.mycontacts.service;
 
 import br.mycontacts.models.Contato;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +21,33 @@ public class AgendaService {
     }
 
     // 3. Buscar por nome
+    public Contato buscarContato(String nome) {
+        String termo = normalizar(nome.toLowerCase());
+
+        boolean encontrado = false;
+
+        for (Contato c : contatos) {
+
+            String nomeContato = normalizar(c.getNome().toLowerCase());
+
+            if (nomeContato.contains(termo)) {
+                return c;
+            }
+            encontrado = true;
+
+        }
+        if (!encontrado) {
+            throw new IllegalArgumentException("Contato não encontrado");
+        }
+    }
 
 
     // 4. Remover contato
+
+    // Normalização de String para pesquisa
+    public String normalizar(String texto) {
+        texto = Normalizer.normalize(texto, Normalizer.Form.NFD);
+        return texto.replaceAll("[^\\p{ASCII}]", "");
+    }
 
 }
